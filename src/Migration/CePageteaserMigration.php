0495e1963dba9f292ad31b2809d307c363e93c8d
Thomas Körner <t.koerner@heimrich-hannot.de>
2022-03-01 14:57:52 +0100

    added page teaser migration

---
+++ src/Migration/CePageteaserMigration.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace HeimrichHannot\ContaoTeaserBundle\Migration;
+
+use Contao\ContentModel;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Migration\MigrationInterface;
+use Contao\CoreBundle\Migration\MigrationResult;
+use HeimrichHannot\ContaoTeaserBundle\ContentElement\LinkTeaserElement;
+
+class CePageteaserMigration implements MigrationInterface
+{
+    private ContaoFramework $contaoFramework;
+
+    public function __construct(ContaoFramework $contaoFramework)
+    {
+        $this->contaoFramework = $contaoFramework;
+    }
+
+    public function getName(): string
+    {
+        return "ce_page_teaser Migration";
+    }
+
+    public function shouldRun(): bool
+    {
+        $this->contaoFramework->initialize();
+        if (ContentModel::findByType('page_teaser')) {
+            return true;
+        }
+        if (ContentModel::findByType('teaser')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function run(): MigrationResult
+    {
+        $pageTeaser = ContentModel::findByType('page_teaser');
+        $articleTeaser = ContentModel::findByType('teaser');
+
+        $pageTeaserCount = 0;
+        $articleTeaserCount = 0;
+
+        if ($pageTeaser) {
+            foreach ($pageTeaser as $teaser)
+            {
+                $teaser->type = LinkTeaserElement::TYPE;
+                $teaser->source = LinkTeaserElement::SOURCE_PAGE;
+                $teaser->jumpTo = $teaser->page_teaser_page;
+                if (!$teaser->showMore) {
+                    $teaser->teaserLinkBehaviour = LinkTeaserElement::LINK_BEHAVIOUR_HIDE_LINK;
+                } else {
+                    $teaser->teaserLinkBehaviour = LinkTeaserElement::LINK_BEHAVIOUR_SHOW_LINK;
+                }
+                $teaser->save();
+                $pageTeaserCount++;
+            }
+        }
+
+        if ($articleTeaser)
+        {
+            foreach ($articleTeaser as $teaser)
+            {
+                if (!$teaser->teaser_page_link && !$teaser->teaser_fragment_identifier)
+                {
+                    continue;
+                }
+                $teaser->type   = LinkTeaserElement::TYPE;
+                $teaser->source = LinkTeaserElement::SOURCE_ARTICLE;
+                $teaser->save();
+                $articleTeaserCount++;
+            }
+        }
+
+        return new MigrationResult(true, "ce_page_teaser Migration successfull! Migrated $pageTeaserCount page_teaser and $articleTeaserCount teaser elements.");
+    }
+}
