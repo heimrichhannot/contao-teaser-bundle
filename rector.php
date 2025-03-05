b6cd48bf3df86af63458c264e9df34431b1b013e
Thomas Körner <t.koerner@heimrich-hannot.de>
2025-03-05 10:41:12 +0100

    move and run rector

--- rector.php
+++ rector.php
@@ -5,26 +5,30 @@ declare(strict_types=1);
 use Contao\Rector\Set\ContaoLevelSetList;
 use Contao\Rector\Set\ContaoSetList;
 use Rector\Config\RectorConfig;
+use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/../../src',
+        __DIR__ . '/src',
+//        __DIR__ . '/contao',
     ])
-    // uncomment to reach your current PHP version
-    // ->withPhpSets()
     ->withRules([
         AddVoidReturnTypeWhereNoReturnRector::class,
+        # In Vorbereitung für PHP 8.4:
+        // ExplicitNullableParamTypeRector::class
     ])
+
+    ->withImportNames(importShortClasses: false, removeUnusedImports: true)
     ->withSets([
-        SetList::PHP_81,
-        LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_74,
         SymfonySetList::SYMFONY_44,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        ContaoSetList::CONTAO_49,
-        ContaoSetList::FQCN,
+        # Erst mit Symfony 6 (Contao 5) nutzen:
+        // SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
         ContaoLevelSetList::UP_TO_CONTAO_49,
+        ContaoSetList::FQCN,
+//        ContaoSetList::ANNOTATIONS_TO_ATTRIBUTES,
     ]);
